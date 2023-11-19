4dee78dcfc3ddce599a54df49fec7e02c182aa38
AbelardoOk <Abelardoemailprivado@gmail.com>
2023-11-19 12:43:15 -0400

    :sparkles: Criação da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section className="h-screen w-screen">
-        <h1 className="text-clip text-center text-6xl font-bold">
+      <section className="relative flex h-screen w-screen flex-col justify-center overflow-hidden">
+        <iframe
+          className="absolute z-20 drop-shadow-xl"
+          src="https://my.spline.design/untitled-eeb93e7ac151a1def4843e6ae82b697d/"
+          width="100%"
+          height="120%"
+        ></iframe>
+        <h1 className="absolute z-10 text-clip px-44 text-center text-9xl font-black drop-shadow-lg">
           O seu Guia de Sobrevivência no Mundo dos Blocos
         </h1>
+
+        <Image
+          className="z-0 blur-sm brightness-75"
+          src="/wallpaper1.jpg"
+          width={1920}
+          height={1080}
+          alt=""
+        />
       </section>
     </main>
   );
