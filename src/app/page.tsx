dda16ba18645335130184e667bc0cc86263e684e
AbelardoOk <Abelardoemailprivado@gmail.com>
2023-11-19 11:11:44 -0400

    :tada: Configurações iniciais do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="h-screen w-screen">
+        <h1 className="text-clip text-center text-6xl font-bold">
+          O seu Guia de Sobrevivência no Mundo dos Blocos
+        </h1>
+      </section>
+    </main>
+  );
+}
